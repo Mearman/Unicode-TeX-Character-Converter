d85d112c7a6b9e13a5741403bb2782901608d787
Joseph Mearman <joseph@mearman.co.uk>
2024-03-01 12:00:37 +0000

    Refactor code to separate functions into individual files

---
+++ src/convertTexToCodepoint.ts
@@ -0,0 +1,14 @@
+import { codePointTex } from "./characters/unicode_to_tex";
+
+export function convertTexToCodepoint(tex: string): string {
+	for (const [unicode, texes] of Object.entries(codePointTex)) {
+		if (Array.isArray(texes)) {
+			if (texes.includes(tex)) {
+				return unicode;
+			}
+		} else if (texes === tex) {
+			return unicode;
+		}
+	}
+	return tex;
+}
