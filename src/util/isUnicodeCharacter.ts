cf9e2ab183d3ee8809d89477866a452717dff547
Joseph Mearman <joseph@mearman.co.uk>
2024-03-03 22:12:22 +0000

    Refactor isUnicodeCharacter function to use charCodeAt method

--- src/util/isUnicodeCharacter.ts
+++ src/util/isUnicodeCharacter.ts
@@ -1,8 +1,7 @@
-import { isDoubleByte } from "./isDoubleByte";
-
 export function isUnicodeCharacter(unicode: `${string}`): boolean {
 	if (unicode.length !== 1) {
 		throw new Error(`Invalid unicode character: ${unicode}`);
 	}
-	return isDoubleByte(unicode);
+	const charCode = unicode.charCodeAt(0);
+	return charCode > 127;
 }
