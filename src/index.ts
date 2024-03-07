b1626b3fa2b985e9f9ac66a930a0d2b21735a804
Joseph Mearman <joseph@mearman.co.uk>
2024-03-07 20:33:39 +0000

    Add cleanForBib utility function

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export {
 	UnicodeTeXCommand,
 	UnicodeTeXCommandMappings,
 } from "./util/UnicodeTeXCommand";
+export { cleanForBib } from "./util/cleanForBib";
 export { decodeString } from "./util/decodeString";
 export { encodeForBib } from "./util/encodeForBib";
 export { encodeString } from "./util/encodeString";
