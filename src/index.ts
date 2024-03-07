628e5ff4976569c9ebbc4eb3648f40b20fde7e9b
Joseph Mearman <joseph@mearman.co.uk>
2024-03-07 10:53:36 +0000

    Add UnicodeTexCommandMappings export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * as util from "./util";
 export { UnicodeTeXCommand } from "./util/UnicodeTeXCommand";
+export { UnicodeTexCommandMappings } from "./util/UnicodeTexCommandMappings";
 export { decodeString } from "./util/decodeString";
 export { encodeString } from "./util/encodeString";
+
