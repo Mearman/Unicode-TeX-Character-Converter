c22cded698015ed7cce93dc3def7c102e59cc9c5
Joseph Mearman <joseph@mearman.co.uk>
2024-03-07 10:49:02 +0000

    Add util module and export isCombiningDiacritic function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * as util from "./util";
 export { UnicodeTeXCommand } from "./util/UnicodeTeXCommand";
 export { decodeString } from "./util/decodeString";
 export { encodeString } from "./util/encodeString";
---
+++ src/util/index.ts
@@ -0,0 +1,1 @@
+export { isCombiningDiacritic } from "./isCombiningDiactricMark";
