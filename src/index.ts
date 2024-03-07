8d6897fb4db7c7dbd00948d8f5be15e6e63ec31e
Joseph Mearman <joseph@mearman.co.uk>
2024-03-07 11:18:50 +0000

    Revert "Add UnicodeTexCommandMappings export"
    This reverts commit 628e5ff4976569c9ebbc4eb3648f40b20fde7e9b.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 export * as util from "./util";
 export { UnicodeTeXCommand } from "./util/UnicodeTeXCommand";
-export { UnicodeTexCommandMappings } from "./util/UnicodeTexCommandMappings";
 export { decodeString } from "./util/decodeString";
 export { encodeString } from "./util/encodeString";
-
