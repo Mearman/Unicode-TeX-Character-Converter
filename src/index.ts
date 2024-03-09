f864c9012f0aa5b45230dffada6a22c424a0b036
Joseph Mearman <joseph@mearman.co.uk>
2024-03-09 16:58:57 +0000

    Add months module

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export {
 	UnicodeTeXCommandMappings,
 } from "./util/UnicodeTeXCommand";
 export { cleanForBib } from "./util/cleanForBib";
+export * as months from "./util/convertMonth";
 export { decodeString } from "./util/decodeString";
 export { encodeForBib } from "./util/encodeForBib";
 export { encodeString } from "./util/encodeString";
